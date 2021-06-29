503e37dcc67d858713faea88930e52fa730f275e
Garfonso <garfonso@mobo.info>
2021-06-29 12:20:26 +0200

    fix test

--- test/integration.js
+++ test/integration.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { tests } = require('@iobroker/testing');
-const integrationTests = require('./integrationTests');
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.integration(path.join(__dirname, '..'), {
